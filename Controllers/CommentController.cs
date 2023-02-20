2a662013ee33d39a2989ccd2ead685ef935a5f9e
Alyona Melekhova <alenamelehova13@gmail.com>
2023-02-20 21:09:26 +0700

    Добавлена страница с комментариями.

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -7,60 +7,103 @@ using System.Security.Claims;
 
 namespace EduSciencePro.Controllers
 {
-   public class CommentController : Controller
-   {
-      private readonly ICommentRepository _comments;
-      private readonly IUserRepository _users;
+    public class CommentController : Controller
+    {
+        private readonly ICommentRepository _comments;
+        private readonly IUserRepository _users;
+        private readonly IPostRepository _posts;
 
-      public CommentController(ICommentRepository comments, IUserRepository users)
-      {
-         _comments = comments;
-         _users = users;
-      }
+        public CommentController(ICommentRepository comments, IUserRepository users, IPostRepository posts)
+        {
+            _comments = comments;
+            _users = users;
+            _posts = posts;
+        }
 
-      [HttpPost]
-      [Route("AddComment")]
-      public async Task<IActionResult> AddComment(AddCommentViewModel model)
-      {
-         //if (!ModelState.IsValid)
-         //{
-         //   foreach (var key in ModelState.Keys)
-         //   {
-         //      if (ModelState[key].Errors.Count > 0)
-         //         ModelState.AddModelError($"{key}", $"{ModelState[key].Errors[0].ErrorMessage}");
-         //   }
-         //   return RedirectToAction("LookingPost", "Post", new {postId = model.PostId});
-         //}
+        [HttpGet]
+        [Route("Comments")]
+        public async Task<IActionResult> Comments()
+        {
+            ClaimsIdentity ident = HttpContext.User.Identity as ClaimsIdentity;
+            var claimEmail = ident.Claims.FirstOrDefault(u => u.Type == ClaimTypes.Name).Value;
+            var user = await _users.GetUserByEmail(claimEmail);
 
-         if (model.Content == null || model.Content.Replace(" ", "") == "")
-         {
-            ModelState.AddModelError("AddCommentViewModel.Content", "Данное поле обязательно для заполнения");
+            List<CommentViewModel> comments = new();
+
+            var posts = await _posts.GetPostViewModelsByUserId(user.Id);
+
+            foreach (var post in posts)
+            {
+                var commentsPost = await _comments.GetCommentViewModelsByPostId(post.Id);
+                comments = comments.Concat(commentsPost).ToList();
+            }
+            return View(comments.Take(5).ToArray());
+        }
+
+        [HttpPost]
+        [Route("CommentsByUserMore/{take}/{skip}")]
+        public async Task<CommentViewModel[]> CommentsByUserMore([FromRoute] int take, [FromRoute] int skip)
+        {
+            ClaimsIdentity ident = HttpContext.User.Identity as ClaimsIdentity;
+            var claimEmail = ident.Claims.FirstOrDefault(u => u.Type == ClaimTypes.Name).Value;
+            var user = await _users.GetUserByEmail(claimEmail);
+
+            List<CommentViewModel> comments = new();
+
+            var posts = await _posts.GetPostViewModelsByUserId(user.Id);
+
+            foreach (var post in posts)
+            {
+                var commentsPost = await _comments.GetCommentViewModelsByPostId(post.Id);
+                comments = comments.Concat(commentsPost).ToList();
+            }
+
+            return comments.Take(take).Skip(skip).ToArray();
+        }
+
+        [HttpPost]
+        [Route("AddComment")]
+        public async Task<IActionResult> AddComment(AddCommentViewModel model)
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //   foreach (var key in ModelState.Keys)
+            //   {
+            //      if (ModelState[key].Errors.Count > 0)
+            //         ModelState.AddModelError($"{key}", $"{ModelState[key].Errors[0].ErrorMessage}");
+            //   }
+            //   return RedirectToAction("LookingPost", "Post", new {postId = model.PostId});
+            //}
+
+            if (model.Content == null || model.Content.Replace(" ", "") == "")
+            {
+                ModelState.AddModelError("AddCommentViewModel.Content", "Данное поле обязательно для заполнения");
+                return RedirectToAction("LookingPost", "Post", new { postId = model.PostId });
+            }
+
+            ClaimsIdentity ident = HttpContext.User.Identity as ClaimsIdentity;
+            var claimEmail = ident.Claims.FirstOrDefault(u => u.Type == ClaimTypes.Name).Value;
+            var user = await _users.GetUserByEmail(claimEmail);
+
+            model.UserId = user.Id;
+            await _comments.Save(model);
             return RedirectToAction("LookingPost", "Post", new { postId = model.PostId });
-         }
-
-         ClaimsIdentity ident = HttpContext.User.Identity as ClaimsIdentity;
-         var claimEmail = ident.Claims.FirstOrDefault(u => u.Type == ClaimTypes.Name).Value;
-         var user = await _users.GetUserByEmail(claimEmail);
-
-         model.UserId = user.Id;
-         await _comments.Save(model);
-         return RedirectToAction("LookingPost", "Post", new { postId = model.PostId });
-      }
+        }
 
         [HttpPost]
         [Route("CommentsMore/{postId}/{take}/{skip}")]
         public async Task<CommentViewModel[]> CommentsMore([FromRoute] Guid postId, [FromRoute] int take, [FromRoute] int skip)
         {
             var comments = await _comments.GetCommentViewModelsByPostId(postId);
-            return comments.TakeLast(take).Skip(skip).ToArray();
+            return comments.Take(take).Skip(skip).ToArray();
         }
 
         [HttpGet]
-      [Route("DeleteComment")]
-      public async Task<IActionResult> DeleteComment(Guid commentId, Guid postId)
-      {
-         await _comments.Delete(commentId);
-         return RedirectToAction("LookingPost", "Post", new { postId = postId });
-      }
-   }
+        [Route("DeleteComment")]
+        public async Task<IActionResult> DeleteComment(Guid commentId, Guid postId)
+        {
+            await _comments.Delete(commentId);
+            return RedirectToAction("LookingPost", "Post", new { postId = postId });
+        }
+    }
 }
--- Data/Repos/CommentRepository.cs
+++ Data/Repos/CommentRepository.cs
@@ -7,80 +7,111 @@ using Microsoft.Extensions.Hosting;
 
 namespace EduSciencePro.Data.Repos
 {
-    public class CommentRepository:ICommentRepository
+    public class CommentRepository : ICommentRepository
     {
-      private readonly ApplicationDbContext _db;
-      private readonly IMapper _mapper;
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
 
-      public CommentRepository(ApplicationDbContext db, IMapper mapper)
-      {
-         _db = db;
-         _mapper = mapper;
-      }
+        public CommentRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
 
-      public async Task<Comment[]> GetCommentsByPostId(Guid postId)
-      {
-      return await _db.Comments.OrderByDescending(p => p.CreatedDate).Where(c => c.PostId == postId).ToArrayAsync();
-      }
+        public async Task<CommentViewModel[]> GetCommentViewModelsByUserId(Guid userId, int take = 5, int skip = 0)
+        {
+            var comments = await _db.Comments.Where(c => c.UserId == userId).OrderByDescending(c => c.CreatedDate).ToListAsync();
+            comments = comments.Take(take).SkipLast(skip).ToList();
+            var commentViewModels = new CommentViewModel[comments.Count()];
+            int i = 0;
+            foreach (var comm in comments)
+            {
+                commentViewModels[i] = _mapper.Map<Comment, CommentViewModel>(comm);
+                commentViewModels[i].User = await _db.Users.FirstOrDefaultAsync(u => u.Id == comm.UserId);
 
-      public async Task<CommentViewModel[]> GetCommentViewModelsByPostId(Guid postId)
-      {
-         var comments = await GetCommentsByPostId(postId);
-         var commentViewModels = new CommentViewModel[comments.Length];
-         int i = 0;
-         foreach (var comm in comments)
-         {
-            commentViewModels[i] = _mapper.Map<Comment, CommentViewModel>(comm);
-            commentViewModels[i].User = await _db.Users.FirstOrDefaultAsync(u => u.Id == comm.UserId);
+                var day = comm.CreatedDate.Day.ToString();
+                var month = comm.CreatedDate.Month.ToString();
+                string date = "";
 
-            var day = comm.CreatedDate.Day.ToString();
-            var month = comm.CreatedDate.Month.ToString();
-            string date = "";
+                if (day.Length == 1)
+                    date += "0" + day + ".";
+                else
+                    date += day + ".";
 
-            if (day.Length == 1)
-               date += "0" + day + ".";
-            else
-               date += day + ".";
+                if (month.Length == 1)
+                    date += "0" + month + ".";
+                else
+                    date += month + ".";
+                date += comm.CreatedDate.Year;
+                commentViewModels[i++].CreatedDate = date;
+            }
+            return commentViewModels;
+        }
 
-            if (month.Length == 1)
-               date += "0" + month + ".";
-            else
-               date += month + ".";
-            date += comm.CreatedDate.Year;
-            commentViewModels[i++].CreatedDate = date;
-         }
-         return commentViewModels;
-      }
+        public async Task<Comment[]> GetCommentsByPostId(Guid postId)
+        {
+            return await _db.Comments.OrderByDescending(p => p.CreatedDate).Where(c => c.PostId == postId).ToArrayAsync();
+        }
 
-      public async Task Save(AddCommentViewModel model)
-      {
-         var comment = _mapper.Map<AddCommentViewModel, Comment>(model);
-         comment.CreatedDate = DateTime.Now;
+        public async Task<CommentViewModel[]> GetCommentViewModelsByPostId(Guid postId)
+        {
+            var comments = await GetCommentsByPostId(postId);
+            var commentViewModels = new CommentViewModel[comments.Length];
+            int i = 0;
+            foreach (var comm in comments)
+            {
+                commentViewModels[i] = _mapper.Map<Comment, CommentViewModel>(comm);
+                commentViewModels[i].User = await _db.Users.FirstOrDefaultAsync(u => u.Id == comm.UserId);
 
-         var entry = _db.Entry(comment);
-         if (entry.State == EntityState.Detached)
-            await _db.Comments.AddAsync(comment);
-         await _db.SaveChangesAsync();
-      }
+                var day = comm.CreatedDate.Day.ToString();
+                var month = comm.CreatedDate.Month.ToString();
+                string date = "";
 
-      public async Task Delete(Guid commentId)
-      {
-         var comment = await _db.Comments.FirstOrDefaultAsync(c => c.Id == commentId);
-         if (comment != null)
-         {
-         _db.Comments.Remove(comment);
+                if (day.Length == 1)
+                    date += "0" + day + ".";
+                else
+                    date += day + ".";
+
+                if (month.Length == 1)
+                    date += "0" + month + ".";
+                else
+                    date += month + ".";
+                date += comm.CreatedDate.Year;
+                commentViewModels[i++].CreatedDate = date;
+            }
+            return commentViewModels;
+        }
+
+        public async Task Save(AddCommentViewModel model)
+        {
+            var comment = _mapper.Map<AddCommentViewModel, Comment>(model);
+            comment.CreatedDate = DateTime.Now;
+
+            var entry = _db.Entry(comment);
+            if (entry.State == EntityState.Detached)
+                await _db.Comments.AddAsync(comment);
             await _db.SaveChangesAsync();
-         }
-      }
-   }
+        }
+
+        public async Task Delete(Guid commentId)
+        {
+            var comment = await _db.Comments.FirstOrDefaultAsync(c => c.Id == commentId);
+            if (comment != null)
+            {
+                _db.Comments.Remove(comment);
+                await _db.SaveChangesAsync();
+            }
+        }
+    }
 
     public interface ICommentRepository
     {
-      //Task<Comment[]> GetComments();
-      Task<Comment[]> GetCommentsByPostId(Guid postId);
-      Task<CommentViewModel[]> GetCommentViewModelsByPostId(Guid postId);
-      Task Save(AddCommentViewModel comment);
-      //Task Update(Comment comment);
-      Task Delete(Guid commentId);
-   }
+        //Task<Comment[]> GetComments();
+        Task<CommentViewModel[]> GetCommentViewModelsByUserId(Guid userId, int take = 5, int skip = 0);
+        Task<Comment[]> GetCommentsByPostId(Guid postId);
+        Task<CommentViewModel[]> GetCommentViewModelsByPostId(Guid postId);
+        Task Save(AddCommentViewModel comment);
+        //Task Update(Comment comment);
+        Task Delete(Guid commentId);
+    }
 }
